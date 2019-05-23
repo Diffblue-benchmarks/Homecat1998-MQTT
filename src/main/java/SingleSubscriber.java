99f1568d63b0fb4fd3a39a6b8024ebff9967cf49
Zhetao Zhong <u6211344@anu.edu.au>
2019-05-23 20:19:09 +1000

    file write and read finish

--- src/main/java/SingleSubscriber.java
+++ src/main/java/SingleSubscriber.java
@@ -1,2 +1,30 @@
+import java.io.*;
+
 public class SingleSubscriber {
+    public static void main(String[] args) {
+
+        /* 读入TXT文件 */
+        String pathname = "data.txt";
+        File filename = new File(pathname);
+        InputStreamReader reader = null;
+        try {
+            reader = new InputStreamReader(
+                    new FileInputStream(filename));
+            BufferedReader br = new BufferedReader(reader);
+            String line = "";
+            while ((line = br.readLine()) != null) {
+                int index1 = line.indexOf(':');
+                int index2 = line.indexOf('@');
+                System.out.println(line.substring(index1 + 1, index2));
+                System.out.println(Long.parseLong(line.substring(index2 + 1)));
+
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
 }
--- src/main/java/SubscriberMain.java
+++ src/main/java/SubscriberMain.java
@@ -1,7 +1,7 @@
 import org.eclipse.paho.client.mqttv3.*;
 import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
 
-import java.net.URL;
+import java.io.*;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Timer;
@@ -11,12 +11,18 @@ import java.util.regex.Pattern;
 public class SubscriberMain implements MqttCallback {
 
     private ArrayList<String> topics = new ArrayList<String>();
+
+    private ArrayList<String> allData = new ArrayList<String>();
+    private ArrayList<Integer> allDup = new ArrayList<Integer>();
+
     private ArrayList<String> qos0data = new ArrayList<String>();
     private ArrayList<String> qos1data = new ArrayList<String>();
     private ArrayList<String> qos2data = new ArrayList<String>();
+
     private ArrayList<Long> qos0time = new ArrayList<Long>();
     private ArrayList<Long> qos1time = new ArrayList<Long>();
     private ArrayList<Long> qos2time = new ArrayList<Long>();
+
 
     int qos0dup;
     int qos1dup;
@@ -33,6 +39,17 @@ public class SubscriberMain implements MqttCallback {
     double qos0orate;
     double qos1orate;
     double qos2orate;
+    double qos0gap;
+    double qos1gap;
+    double qos2gap;
+
+    File outputPlace;
+    BufferedWriter out;
+
+    InputStreamReader reader;
+    BufferedReader in;
+
+
 
     DecimalFormat df = new DecimalFormat("0.00000");
 
@@ -41,6 +58,23 @@ public class SubscriberMain implements MqttCallback {
     public SubscriberMain(ArrayList<String> topics) throws MqttException {
 
         this.topics = topics;
+
+        outputPlace = new File("data.txt");
+        try {
+            outputPlace.createNewFile();
+        } catch (IOException e) {
+            System.out.println("Error in create data file");
+            System.exit(1);
+            e.printStackTrace();
+        }
+
+        try {
+            out = new BufferedWriter(new FileWriter(outputPlace));
+        } catch (IOException e) {
+            System.out.println("Error in create writer");
+            System.exit(1);
+            e.printStackTrace();
+        }
 
 
         String host = "tcp://comp3310.ddns.net:1883";
@@ -65,12 +99,22 @@ public class SubscriberMain implements MqttCallback {
 
         for (int i = 0; i < topics.size(); i ++){
             this.client.subscribe(this.topics.get(i), i);
+            int dup = 0;
+            allDup.add(dup);
 
         }
     }
 
 
     private void stop() throws MqttException {
+        try {
+            out.flush();
+            out.close();
+        } catch (IOException e) {
+            System.out.println("Fail to close the file");
+            e.printStackTrace();
+        }
+
         this.client.disconnect();
         this.client.close();
     }
@@ -82,29 +126,61 @@ public class SubscriberMain implements MqttCallback {
     }
 
     public void messageArrived(String s, MqttMessage mqttMessage) throws Exception {
-        if (mqttMessage.getQos() == 0){
-            if (mqttMessage.isDuplicate()){
-                qos0dup ++;
-            }
-            qos0data.add(new String(mqttMessage.getPayload()));
-            qos0time.add(System.currentTimeMillis());
-        } else if (mqttMessage.getQos() == 1){
-            if (mqttMessage.isDuplicate()){
-                qos1dup ++;
-            }
-            qos1data.add(new String(mqttMessage.getPayload()));
-            qos1time.add(System.currentTimeMillis());
-        } else if (mqttMessage.getQos() == 2){
-            if (mqttMessage.isDuplicate()){
-                qos2dup ++;
-            }
-            qos2data.add(new String(mqttMessage.getPayload()));
-            qos2time.add(System.currentTimeMillis());
+
+        if (mqttMessage.isDuplicate()){
+            allDup.set(mqttMessage.getQos(), allDup.get(mqttMessage.getQos()) + 1);
         }
+
+        out.write(mqttMessage.getQos() + ":" + new String(mqttMessage.getPayload()) + "@" + System.currentTimeMillis() + "\r\n");
+        allData.add(mqttMessage.getQos() + ":" + new String(mqttMessage.getPayload()) + "@" + System.currentTimeMillis());
+
     }
 
     public void deliveryComplete(IMqttDeliveryToken iMqttDeliveryToken) {
 
+    }
+
+    private void analyze(){
+
+        try {
+            reader = new InputStreamReader(new FileInputStream("data.txt"));
+            in = new BufferedReader(reader);
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found!");
+            e.printStackTrace();
+        }
+
+        try {
+            String line=null;
+            while ((line = in.readLine()) != null && line.length() > 20) {
+                int index1 = line.indexOf(':');
+                int index2 = line.indexOf('@');
+                String data = line.substring(index1 + 1, index2);
+                Long timeStamp = Long.parseLong(line.substring(index2 + 1));
+                System.out.println(data);
+                System.out.println(timeStamp);
+                if (line.charAt(0) == '0'){
+                    qos0data.add(data);
+                    qos0time.add(timeStamp);
+                } else if (line.charAt(0) == '1'){
+                    qos1data.add(data);
+                    qos1time.add(timeStamp);
+                } else if (line.charAt(0) == '2'){
+                    qos2data.add(data);
+                    qos2time.add(timeStamp);
+                }
+            }
+            in.close();
+
+
+        } catch (IOException e) {
+            System.out.println("Error in reading the file");
+            e.printStackTrace();
+        }
+
+        qos0dup = allDup.get(0);
+        qos1dup = allDup.get(1);
+        qos2dup = allDup.get(2);
     }
 
 
@@ -115,20 +191,45 @@ public class SubscriberMain implements MqttCallback {
 
 
 
+
+    /*
+    * Calculate the result
+    * Duplicate Rate - drate
+    * Speed Rate - rate
+    * Out of Order Rate -orate
+    * Inter-data-gap - gap
+    * Standard Deviation - gvar
+    * */
+
     private void calculate(){
 
+        /*
+        * calculate the duplicate rate for each Qos
+        * duplicate rate = number of duplicate / total
+        * */
         qos0drate = (double)qos0dup / (double)qos0data.size();
         qos1drate = (double)qos1dup / (double)qos1data.size();
         qos2drate = (double)qos2dup / (double)qos2data.size();
 
-
+        /*
+        * calculate the speed rate for each Qos
+        * speed rate = total number / total time
+        * total time = the timestamp of last message - the timestamp of the first message
+        * */
         qos0rate = (double)1000 * qos0data.size() / (double) (qos0time.get(qos0time.size() - 1) - qos0time.get(0));
         qos1rate = (double)1000 * qos1data.size() / (double) (qos1time.get(qos1time.size() - 1) - qos1time.get(0));
         qos2rate = (double)1000 * qos2data.size() / (double) (qos2time.get(qos2time.size() - 1) - qos2time.get(0));
 
 
-        int last = -1;
 
+        /*
+        * calculate the out of order for each Qos
+        * if the data is number, compare it to the last one
+        * if it is smaller than the last one
+        * then it is out of order
+        * and update the "last" number
+        * */
+        int last = -1;
         for (String qos0datum : qos0data) {
             if (isNumber(qos0datum)) {
                 if (Integer.parseInt(qos0datum) < last) {
@@ -140,7 +241,6 @@ public class SubscriberMain implements MqttCallback {
 
         last = -1;
         for (String qos1datum : qos1data) {
-
             if (isNumber(qos1datum)) {
                 if (Long.parseLong(qos1datum) < last) {
                     qos1ooo++;
@@ -159,26 +259,47 @@ public class SubscriberMain implements MqttCallback {
             }
         }
 
+
+        /*
+        * calculate the out of order rate for each Qos
+        * out-of-order rate = number of ooo / total number
+        * */
         qos0orate = (double) qos0ooo / (double) qos0data.size();
         qos1orate = (double) qos1ooo / (double) qos1data.size();
         qos2orate = (double) qos2ooo / (double) qos2data.size();
 
 
+        /*
+        * calculate the inter-message-gap for each Qos
+        * inter-message-gap = the timestamp of last message - the timestamp of the first message  / total number
+        * */
+        qos0gap = (double) (qos0time.get(qos0time.size() - 1) - qos0time.get(0)) / (double)qos0data.size();
+        qos1gap = (double) (qos1time.get(qos1time.size() - 1) - qos1time.get(0)) / (double)qos1data.size();
+        qos2gap = (double) (qos2time.get(qos2time.size() - 1) - qos2time.get(0)) / (double)qos2data.size();
+
     }
 
 
+
+    /*
+    * print out the result
+    * */
     private void print(){
-        System.out.println("Qos0 receive: " + qos0data.size() + ", with: " + df.format(qos0drate) + "% duplicate.");
+        System.out.println("\nQos0 receive: " + qos0data.size() + ", with: " + df.format(qos0drate) + "% duplicate.");
         System.out.println("Qos1 receive: " + qos1data.size() + ", with: " + df.format(qos1drate) + "% duplicate.");
         System.out.println("Qos2 receive: " + qos2data.size() + ", with: " + df.format(qos2drate) + "% duplicate.");
 
-        System.out.println("Qos0 rate: " + df.format(qos0rate) + " per sec.");
+        System.out.println("\nQos0 rate: " + df.format(qos0rate) + " per sec.");
         System.out.println("Qos1 rate: " + df.format(qos1rate) + " per sec.");
         System.out.println("Qos2 rate: " + df.format(qos2rate) + " per sec.");
 
-        System.out.println("Qos0 out-of-order rate: " + df.format(qos0orate) + "%");
+        System.out.println("\nQos0 out-of-order rate: " + df.format(qos0orate) + "%");
         System.out.println("Qos1 out-of-order rate: " + df.format(qos1orate) + "%");
         System.out.println("Qos2 out-of-order rate: " + df.format(qos2orate) + "%");
+
+        System.out.println("\nQos0 inter-message-gap: " + df.format(qos0gap) + "%");
+        System.out.println("Qos1 inter-message-gap: " + df.format(qos1gap) + "%");
+        System.out.println("Qos2 inter-message-gap: " + df.format(qos2gap) + "%");
     }
 
 
@@ -195,21 +316,22 @@ public class SubscriberMain implements MqttCallback {
 
         Timer timer = new Timer();
 
-        TimerTask timerTask = new TimerTask() {
+        final TimerTask timerTask = new TimerTask() {
             @Override
             public void run() {
                 try {
+                    subscriberMain.analyze();
                     subscriberMain.stop();
                     subscriberMain.calculate();
                     subscriberMain.print();
-                    System.exit(0);
+                    cancel();
                 } catch (MqttException e) {
                     e.printStackTrace();
                 }
             }
         };
 
-        timer.schedule(timerTask, 30000);
+        timer.schedule(timerTask, 100000);
 
     }
 }
